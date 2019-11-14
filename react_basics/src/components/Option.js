606f8cd600c969d2229956731fa1fd276ff0bd5d
Paul Chesa <paulchesa1@gmail.com>
2019-11-14 11:16:47 +0300

    add css on modal
    Sample ToDo App without db connection

--- react_basics/src/components/Option.js
+++ react_basics/src/components/Option.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Option = (props) => (
-    <div>
+    
     <div className = "option"> 
     <p className= "option_text">{props.count}.{props.optionText} </p>
-    </div>
+    
     <button className="button button--link"
     onClick = {(e) => {
         props.handleRemove(props.optionText)
